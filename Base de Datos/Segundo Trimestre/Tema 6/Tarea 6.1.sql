8962c3818a264f0bf109a4dc3141e7c8236ab852
César Valverde Pardo <2899222@alu.murciaeduca.es>
2026-02-18 04:19:46 +0100

    Tema 5 tareas hechas

---
+++ Base de Datos/Segundo Trimestre/Tema 6/Tarea 6.1.sql
@@ -0,0 +1,43 @@
+-- Consulta 1
+-- SELECT *
+-- 	FROM products
+-- 	WHERE buyPrice > 50;
+
+-- Consulta 2
+
+-- SELECT DISTINCT *
+-- FROM products;
+    
+-- Consulta 3
+-- SELECT productName, 
+-- 	productLine
+-- FROM products
+-- WHERE productLine = 'Classic Cars' AND quantityInStock <1000;
+
+-- Consulta 4
+-- SELECT *
+-- FROM products
+-- ORDER BY MSRP ASC LIMIT 10;
+
+-- Consulta 5
+-- SELECT *
+-- FROM employees
+-- WHERE lastName LIKE 'P%';
+
+-- Consulta 6
+-- SELECT firstName,
+-- 		lastName
+-- FROM employees
+-- WHERE officeCode = 1;
+
+-- Consulta 7
+-- SELECT customerNAME,
+-- 		creditLimit
+-- FROM customers
+-- WHERE country = 'SPAIN' AND creditLimit > 100000
+-- ORDER BY customerName;
+
+-- Consulta 8
+
+
+
